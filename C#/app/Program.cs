60723a98b10b42ca06d2b1fe80052d61664a5f06
Khanghao <57928653+Khanghao@users.noreply.github.com>
2023-06-23 15:04:08 +0700

    Write a function that will accept a sentence (String) and return the average number of letters per word (Int) for words that are at least 2 characters or longer.

---
+++ C#/app/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Nhập câu:");
+        string sentence = Console.ReadLine();
+        int averageLetters = Check(sentence);
+        Console.WriteLine("Trung bình số chữ cái trên mỗi từ là: " + averageLetters);
+    }
+
+    public static int Check(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+        int totalLetters = 0;
+        int validWordCount = 0;
+
+        foreach (string word in words)
+        {
+            if (word.Length >= 2)
+            {
+                totalLetters += word.Length;
+                validWordCount++;
+            }
+        }
+
+        return validWordCount > 0 ? totalLetters / validWordCount : 0;
+    }
+}
